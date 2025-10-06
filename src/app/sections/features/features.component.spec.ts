ccd2846c6cef87f63385a06dea568d4bb32c3d2a
Ot Roca <otrocadev@gmail.com>
2025-10-06 19:00:44 +0200

    test: add unit tests for features navigation menu rendering

--- src/app/sections/features/features.component.spec.ts
+++ src/app/sections/features/features.component.spec.ts
@@ -8,9 +8,8 @@ describe('FeaturesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeaturesComponent]
-    })
-    .compileComponents();
+      imports: [FeaturesComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(FeaturesComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,17 @@ describe('FeaturesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the features nav', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const featuresNav = compiled.querySelector('#features-nav');
+    expect(featuresNav).toBeTruthy();
+
+    const menuElements = featuresNav?.querySelectorAll('.menu-element');
+    expect(menuElements?.length).toBe(3);
+    expect(menuElements?.[0].textContent).toContain('Simple Bookmarking');
+    expect(menuElements?.[1].textContent).toContain('Speedy Searching');
+    expect(menuElements?.[2].textContent).toContain('Easy Sharing');
   });
 });
