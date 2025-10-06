fd02bd692388c0bb3c31bbaeee515f13f5cf5db9
Ot Roca <otrocadev@gmail.com>
2025-10-06 18:42:06 +0200

    feat: add features section with responsive layout and tab navigation (pending responsiveness)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './sections/hero/hero.component';
+import { FeaturesComponent } from './sections/features/features.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent],
+  imports: [HeroComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
