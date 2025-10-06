3573bf233cce2683a1e842128d741c2348ec0958
Ot Roca <otrocadev@gmail.com>
2025-10-06 13:24:30 +0200

    test: add hero component tests

--- src/app/sections/hero/hero.component.spec.ts
+++ src/app/sections/hero/hero.component.spec.ts
@@ -8,9 +8,8 @@ describe('HeroComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroComponent]
-    })
-    .compileComponents();
+      imports: [HeroComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
@@ -19,5 +18,20 @@ describe('HeroComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the hero text', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'A Simple Bookmark Manager'
+    );
+  });
+
+  it('should render the hero buttons', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const buttons = compiled.querySelectorAll('button');
+    expect(buttons.length).toBe(2);
+    expect(buttons[0].textContent).toContain('Get it on Chrome');
+    expect(buttons[1].textContent).toContain('Get it on Firefox');
   });
 });
